85fcf32136ea26ae0a67aa143c470378527ebe8d
nam-web <65065658+nam-web@users.noreply.github.com>
2024-06-02 14:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+function updateUrl(event) {
+            event.preventDefault(); // Prevent form submission
+            const n = document.getElementById('name').value;
+            const y = document.getElementById('year').value;
+			const queryString = `?name=${encodeURIComponent(n)}&year=${encodeURIComponent(y)}`;
+			const urlElement = document.getElementById('url');
+            urlElement.textContent = `https://localhost:8080/${queryString}`;
+			
+            
+            
+        }
+
+        window.onload = function() {
+            // Add event listener to the submit button
+            document.getElementById('button').addEventListener('click', updateUrl);
+        }
